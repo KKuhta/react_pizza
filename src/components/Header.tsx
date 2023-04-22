8a20e75e46c6764af162834b394cace137a62dea
KKuhta <kkuhta02@mail.ru>
2023-04-22 19:42:18 +0300

    localStorage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logoSvg from '../assets/img/pizza-logo.svg';
 import { useSelector } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
@@ -7,7 +8,18 @@ import { selectCart } from '../redux/slices/cartSlice';
 function Header() {
   const { items, totalPrice } = useSelector(selectCart);
   const location = useLocation();
+  const isMounted = React.useRef(false);
+
   const totalCount = items.reduce((sum: number, item: any) => sum + item.count, 0);
+
+  React.useEffect(() => {
+    if (isMounted.current) {
+      const json = JSON.stringify(items);
+      localStorage.setItem('cart', json);
+    }
+    isMounted.current = true;
+  }, [items]);
+
   return (
     <div className="header">
       <div className="container">
